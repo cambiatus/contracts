0792ea043cbd9120446e2c07e565197a4fab819b
Julien Lucca <julienlucca@gmail.com>
2020-09-10 20:35:55 -0300

    Feature/parametrization (#17)
    * update .gitignore
    
    * Renaming, reformating
    
    * Update gitignore
    
    * Add contract parametrization
    
    * Add community wasm
    
    * Rename community contract, update authorization
    
    * Update wasm files
    
    * Update makefile to add new variable
    
    * Improve authorization on initacc action

--- community/community.hpp
+++ community/community.hpp
@@ -5,7 +5,7 @@
 #define TOSTR_(T) #T
 #define TOSTR(T) TOSTR_(T)
 
-class [[eosio::contract("cambiatus.community")]] bespiral : public eosio::contract
+class [[eosio::contract("cambiatus.community")]] cambiatus : public eosio::contract
 {
 public:
   using contract::contract;
@@ -154,7 +154,7 @@ public:
   };
 
   /// @abi action
-  /// Creates a BeSpiral community
+  /// Creates a cambiatus community
   ACTION create(eosio::asset cmm_asset, eosio::name creator, std::string logo, std::string name,
                 std::string description, eosio::asset inviter_reward, eosio::asset invited_reward,
                 std::uint8_t has_objectives, std::uint8_t has_shop);
@@ -243,61 +243,62 @@ public:
   // Get available key
   uint64_t get_available_id(std::string table);
 
-  typedef eosio::multi_index<eosio::name{"community"}, bespiral::community> communities;
+  typedef eosio::multi_index<eosio::name{"community"}, cambiatus::community> communities;
 
   typedef eosio::multi_index<eosio::name{"network"},
-                             bespiral::network,
+                             cambiatus::network,
                              eosio::indexed_by<eosio::name{"usersbycmm"},
-                                               eosio::const_mem_fun<bespiral::network, uint64_t, &bespiral::network::users_by_cmm>>>
+                                               eosio::const_mem_fun<cambiatus::network, uint64_t, &cambiatus::network::users_by_cmm>>>
       networks;
 
   typedef eosio::multi_index<eosio::name{"objective"},
-                             bespiral::objective,
+                             cambiatus::objective,
                              eosio::indexed_by<eosio::name{"bycmm"},
-                                               eosio::const_mem_fun<bespiral::objective, uint64_t, &bespiral::objective::by_cmm>>>
+                                               eosio::const_mem_fun<cambiatus::objective, uint64_t, &cambiatus::objective::by_cmm>>>
       objectives;
 
   typedef eosio::multi_index<eosio::name{"action"},
-                             bespiral::action,
+                             cambiatus::action,
                              eosio::indexed_by<eosio::name{"byobj"},
-                                               eosio::const_mem_fun<bespiral::action, uint64_t, &bespiral::action::by_objective>>>
+                                               eosio::const_mem_fun<cambiatus::action, uint64_t, &cambiatus::action::by_objective>>>
       actions;
 
   typedef eosio::multi_index<eosio::name{"validator"},
-                             bespiral::action_validator,
+                             cambiatus::action_validator,
                              eosio::indexed_by<eosio::name{"byaction"},
-                                               eosio::const_mem_fun<bespiral::action_validator, uint64_t, &bespiral::action_validator::by_action>>>
+                                               eosio::const_mem_fun<cambiatus::action_validator, uint64_t, &cambiatus::action_validator::by_action>>>
       validators;
 
   typedef eosio::multi_index<eosio::name{"claim"},
-                             bespiral::claim,
+                             cambiatus::claim,
                              eosio::indexed_by<eosio::name{"byaction"},
-                                               eosio::const_mem_fun<bespiral::claim, uint64_t, &bespiral::claim::by_action>>>
+                                               eosio::const_mem_fun<cambiatus::claim, uint64_t, &cambiatus::claim::by_action>>>
       claims;
 
   typedef eosio::multi_index<eosio::name{"check"},
-                             bespiral::check,
+                             cambiatus::check,
                              eosio::indexed_by<eosio::name{"byclaim"},
-                                               eosio::const_mem_fun<bespiral::check, uint64_t, &bespiral::check::by_claim>>>
+                                               eosio::const_mem_fun<cambiatus::check, uint64_t, &cambiatus::check::by_claim>>>
       checks;
 
   typedef eosio::multi_index<eosio::name{"sale"},
-                             bespiral::sale,
+                             cambiatus::sale,
                              eosio::indexed_by<eosio::name{"bycmm"},
-                                               eosio::const_mem_fun<bespiral::sale, uint64_t, &bespiral::sale::by_cmm>>,
+                                               eosio::const_mem_fun<cambiatus::sale, uint64_t, &cambiatus::sale::by_cmm>>,
                              eosio::indexed_by<eosio::name{"byuser"},
-                                               eosio::const_mem_fun<bespiral::sale, uint64_t, &bespiral::sale::by_user>>>
+                                               eosio::const_mem_fun<cambiatus::sale, uint64_t, &cambiatus::sale::by_user>>>
       sales;
 
-  typedef eosio::singleton<eosio::name{"indexes"}, bespiral::indexes> item_indexes;
+  typedef eosio::singleton<eosio::name{"indexes"}, cambiatus::indexes> item_indexes;
 
   item_indexes curr_indexes;
 
   // Initialize our singleton table for indices
-  bespiral(eosio::name receiver, eosio::name code, eosio::datastream<const char *> ds) : contract(receiver, code, ds), curr_indexes(_self, _self.value) {}
+  cambiatus(eosio::name receiver, eosio::name code, eosio::datastream<const char *> ds) : contract(receiver, code, ds), curr_indexes(_self, _self.value) {}
 };
 
 const auto currency_account = eosio::name{TOSTR(__TOKEN_ACCOUNT__)};
+const auto backend_account = eosio::name{TOSTR(__BACKEND_ACCOUNT__)};
 
 // Add global reference for a table from the token contract
 struct currency_stats
@@ -310,4 +311,4 @@ struct currency_stats
 
   uint64_t primary_key() const { return supply.symbol.code().raw(); }
 };
-typedef eosio::multi_index<eosio::name{"stat"}, currency_stats> bespiral_tokens;
+typedef eosio::multi_index<eosio::name{"stat"}, currency_stats> cambiatus_tokens;
